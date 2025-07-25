d3805f98d00441092d9cf1b3786c30efba821eaf
samithaathurupana <athurupana1998@gmail.com>
2025-07-26 00:46:20 +0530

    init commit

---
+++ Day3/passwordChecker1000.py
@@ -0,0 +1,12 @@
+#write a password checker. if password match loging successful even fail
+userPass=input("Enter the password: ")
+
+if userPass != "9999":
+    print("Password not matched")
+    exit()
+else:
+    print("Logging successful")
+print("""secret data
+    ___^__________^___
+    ________V_________
+    ____----------____""")
